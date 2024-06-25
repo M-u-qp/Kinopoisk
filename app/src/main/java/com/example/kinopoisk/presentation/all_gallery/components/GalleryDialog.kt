c46072dd08aecd3e53d746e932651071bbc4f1d5
M-u-qp <muqpu4eckui@gmail.com>
2024-06-25 11:56:13 +0300

    fix GalleryDialog

--- app/src/main/java/com/example/kinopoisk/presentation/all_gallery/components/GalleryDialog.kt
+++ app/src/main/java/com/example/kinopoisk/presentation/all_gallery/components/GalleryDialog.kt
@@ -1,22 +1,17 @@
 package com.example.kinopoisk.presentation.all_gallery.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layout
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.kinopoisk.domain.model.GalleryItem
@@ -35,28 +30,16 @@ fun GalleryDialog(
     Dialog(onDismissRequest = {
         showDialog.value = false
         viewModel.updateVisibleGalleryDialog(showDialog.value)
-    }) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .layout { measurable, constraints ->
-                    val placeable = measurable.measure(constraints)
-                    layout(placeable.width, placeable.height) {
-                        placeable.placeRelative(0, 0)
-                    }
-                }
-                .background(color = Color.Black.copy(alpha = 0.5f)),
-            contentAlignment = Alignment.Center
+    },
+        properties = DialogProperties(usePlatformDefaultWidth = false)
         ) {
-        HorizontalPager(modifier = Modifier.fillMaxSize(), state = pagerState) { index ->
-
-                AsyncImage(
-                    modifier = Modifier.fillMaxWidth(),
-                    model = ImageRequest.Builder(context).data(listImages[index].imageUrl).build(),
-                    contentDescription = null,
-
-                    )
-            }
+        HorizontalPager(modifier = Modifier, state = pagerState) { index ->
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                model = ImageRequest.Builder(context).data(listImages[index].imageUrl).build(),
+                contentDescription = null,
+                contentScale = ContentScale.Fit
+            )
         }
     }
 }
